bf43db9e2b1fa3cbb0e07ecf1d24441bb01aa27d
Justin Long <33242991+justin-long@users.noreply.github.com>
2018-08-27 18:52:07 -0400

    Initial Push

---
+++ Data/Zipped/get_zip_files.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\aria2-1.34.0-win-64bit-build1\aria2-1.34.0-win-64bit-build1\aria2c.exe" --dir="C:\Retrosheet\Data\Zipped" --input-file="./retrosheet_zip_files.txt"
+"C:\Program Files\7-Zip\7z.exe" e *.zip -y -oC:\Retrosheet\data\unzipped
